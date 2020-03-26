27755d53e9fdb9cb4c1b72e54000b9c72e4469f9
Timo <tosterkamp@cyberto.de>
2020-03-26 13:39:36 +0100

    Update server lists periodically and check if all servers are online

--- server.py
+++ server.py
@@ -1,8 +1,14 @@
 import random
+import requests
+from datetime import datetime, timedelta
+import threading
+import time
+import logging
+import uwsgi
+
 
 
 def application(env, start_response):
-    print(env['HTTP_HOST'])
     if(env['HTTP_HOST'].startswith('jitsi')):
         redirect_server = jitsi.get_random()
     elif(env['HTTP_HOST'].startswith('poll')):
@@ -27,24 +33,87 @@ def application(env, start_response):
 
 
 class ServerList:
-    def __init__(self, filename):
+    def __init__(self, filename, t_request):
+        """
+        @param filename: name of the file with the serverlist in the folder res
+        @param t_request: file, which is located on the searched servers, which can be used to measure the speed of the server
+        """
+        self.lock = threading.Lock()
         self.file = 'res/' + filename
+        self.test_request = t_request;
         self.servers = []
+        self.offline_servers = []
+        self.renew()
 
     def get_random(self):
-        self.renew()
-        return random.choice(self.servers)
+        """
+        return a random server from self.list
+        """
+        self.lock.acquire()
+        tmp = random.choice(self.servers)
+        self.lock.release()
+        return tmp
 
     def renew(self):
-        self.servers.clear()
-        with open(self.file, 'r') as f:
-            self.servers = f.readlines()
+        """
+        get current list from filesystem and check if the servers are online
+        """
+        if self.test_request is None:
+            with open(self.file, 'r') as f:
+                self.servers = f.readlines()
+        else:
+            online = []
+            offline = []
+            with open(self.file, 'r') as f:
+                all_servers = f.readlines()
+            for x in all_servers:
+                try:
+                    r = requests.get(str.rstrip(x) + self.test_request, timeout=0.5)
+                    if (r.status_code == requests.codes.ok):
+                        #print(str.rstrip(x) + " is online")
+                        online.append(x)
+                    else:
+                        #print(str.rstrip(x) + " returns status code: " + str(r.status_code))
+                        offline.append(x)
+                except Exception as inst:
+                    #print(str.rstrip(x) + " Error: " + str(inst))
+                    offline.append(x)
+            #print("offline: ")
+            #print(offline)
+            self.lock.acquire()
+            self.servers = online
+            self.offline_servers = offline
+            self.lock.release()
+            #print("online: ")
+            #print(self.servers)
 
 
-jitsi = ServerList('jitsi_servers.lst')
-poll = ServerList('poll_servers.lst')
-pad = ServerList('pad_servers.lst')
-codimd = ServerList('codimd_servers.lst')
-cryptpad = ServerList('cryptpad_servers.lst')
-etherpad = ServerList('etherpad_servers.lst')
-ethercalc = ServerList('ethercalc_servers.lst')
+
+jitsi = ServerList('jitsi_servers.lst', 'sounds/outgoingRinging.wav')
+poll = ServerList('poll_servers.lst', 'images/date.png')
+pad = ServerList('pad_servers.lst', None)
+codimd = ServerList('codimd_servers.lst', 'screenshot.png')
+cryptpad = ServerList('cryptpad_servers.lst', 'customize/images/AGPL.png')
+etherpad = ServerList('etherpad_servers.lst', 'locales.json')
+ethercalc = ServerList('ethercalc_servers.lst', 'static/img/davy/bg/home2.png')
+
+def reload(signum):
+    print("start reload")
+    global jitsi
+    global poll
+    global pad
+    global codimd
+    global cryptpad
+    global etherpad
+    global ethercalc
+    jitsi.renew()
+    poll.renew()
+    pad.renew()
+    codimd.renew()
+    cryptpad.renew()
+    etherpad.renew()
+    ethercalc.renew()
+    print("finish reload")
+
+uwsgi.register_signal(99, "", reload)
+uwsgi.add_timer(99, 600)
