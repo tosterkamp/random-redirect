118b8c127babc5bc3aafc0fe1cf0826d8cf81ccf
Timo Osterkamp <tosterkamp@uos.de>
2020-05-29 15:54:30 +0200

    add return server list query

--- server.py
+++ server.py
@@ -10,28 +10,31 @@ import server_utils
 
 
 def application(env, start_response):
-    if(env['HTTP_HOST'].startswith('jitsi')):
-        redirect_server = jitsi.get_random(env['QUERY_STRING'])
-    elif(env['HTTP_HOST'].startswith('poll')):
-        redirect_server = poll.get_random(env['QUERY_STRING'])
-    elif(env['HTTP_HOST'].startswith('pad')):
-        redirect_server = pad.get_random()
-    elif(env['HTTP_HOST'].startswith('codimd')):
-        redirect_server = codimd.get_random(env['QUERY_STRING'])
-    elif(env['HTTP_HOST'].startswith('cryptpad')):
-        redirect_server = cryptpad.get_random(env['QUERY_STRING'])
-    elif(env['HTTP_HOST'].startswith('etherpad')):
-        redirect_server = etherpad.get_random(env['QUERY_STRING'])
-    elif(env['HTTP_HOST'].startswith('ethercalc')):
-        redirect_server = ethercalc.get_random(env['QUERY_STRING'])
-    elif(env['HTTP_HOST'].startswith('bbb')):
-        redirect_server = bbb.get_random()
-    else:
-        redirect_server = 'https://github.com/tosterkamp/random-redirect/'
-    
-    print('redirect from ' + env['HTTP_HOST'] + ' to ' + redirect_server)
-    start_response('302', [('Location', redirect_server)])
-    return [b"redirected"] 
+    #if(env['HTTP_HOST'].startswith('jitsi')):
+    #    redirect_server = jitsi.get_random(env['QUERY_STRING'])
+    #elif(env['HTTP_HOST'].startswith('poll')):
+    #    redirect_server = poll.get_random(env['QUERY_STRING'])
+    #elif(env['HTTP_HOST'].startswith('pad')):
+    #    redirect_server = pad.get_random()
+    #elif(env['HTTP_HOST'].startswith('codimd')):
+    #    redirect_server = codimd.get_random(env['QUERY_STRING'])
+    #elif(env['HTTP_HOST'].startswith('cryptpad')):
+    #    redirect_server = cryptpad.get_random(env['QUERY_STRING'])
+    #elif(env['HTTP_HOST'].startswith('etherpad')):
+    #    redirect_server = etherpad.get_random(env['QUERY_STRING'])
+    #elif(env['HTTP_HOST'].startswith('ethercalc')):
+    #    redirect_server = ethercalc.get_random(env['QUERY_STRING'])
+    #elif(env['HTTP_HOST'].startswith('bbb')):
+    #    redirect_server = bbb.get_random()
+    #else:
+    #    redirect_server = 'https://github.com/tosterkamp/random-redirect/'
+    # 
+    #print('redirect from ' + env['HTTP_HOST'] + ' to ' + redirect_server)
+    #start_response('302', [('Location', redirect_server)])
+    #return [b"redirected"]
+    status = '200 OK'
+    headers = [('Content-type', 'text/plain')]
+    return [jitsi.get_list(env['QUERY_STRING'])]
 
 
 
@@ -84,6 +87,24 @@ class ServerList:
         self.lock.release()
         return tmp
 
+    def get_list(self, params=""):
+        allowBadHoster = False
+        allowBadStun = False
+        allowOnlyCountry = []
+        if (params.casefold().find("allowBadHoster".casefold()) != -1):
+            allowBadHoster = True
+        if (params.casefold().find("allowBadStun".casefold()) != -1):
+            allowBadStun = True
+        if (params.casefold().find("country-code".casefold()) != -1):
+            splits = params.casefold().split("&")
+            for item in splits:
+                if (item.find("country-code".casefold()) != -1):
+                    allowOnlyCountry.append((item.split("=",1)[1]).casefold())
+
+        tmp_list = dict(filter(lambda (url, badHoster, badStun, country_code)): (not (not allowBadHoster) and self.properties[i]["badHoster"]) and (not ((not allowBadStun) and self.properties[i]["badStun"])) and (self.properties[i]["country_code"].casefold() in allowOnlyCountry), self.properties.items()))
+        
+        return tmp_dict
+
 
     def renew(self):
         """
@@ -115,6 +136,7 @@ class ServerList:
             for i in range(len(online)):
                 props.append(1)
                 tmp_dict = {
+                    "url": online[i],
                     "badHoster": server_utils.hasBadHoster(online[i]),
                     "badStun": server_utils.hasBadStun(online[i]),
                     "country_code": server_utils.getCountry(online[i])
@@ -134,33 +156,32 @@ class ServerList:
 
 
 jitsi = ServerList('jitsi_servers.lst', 'sounds/outgoingRinging.wav')
-poll = ServerList('poll_servers.lst', 'images/date.png')
-pad = ServerList('pad_servers.lst', None)
-codimd = ServerList('codimd_servers.lst', 'screenshot.png')
-cryptpad = ServerList('cryptpad_servers.lst', 'customize/images/AGPL.png')
-etherpad = ServerList('etherpad_servers.lst', 'locales.json')
-ethercalc = ServerList('ethercalc_servers.lst', 'static/img/davy/bg/home2.png')
-#bbb = ServerList('bbb_servers.lst', 'html5client/resources/sounds/LeftCall.mp3')
-bbb = ServerList('bbb_servers.lst', None)
+#poll = ServerList('poll_servers.lst', 'images/date.png')
+#pad = ServerList('pad_servers.lst', None)
+#codimd = ServerList('codimd_servers.lst', 'screenshot.png')
+#cryptpad = ServerList('cryptpad_servers.lst', 'customize/images/AGPL.png')
+#etherpad = ServerList('etherpad_servers.lst', 'locales.json')
+#ethercalc = ServerList('ethercalc_servers.lst', 'static/img/davy/bg/home2.png')
+#bbb = ServerList('bbb_servers.lst', None)
 
 def reload(signum):
     print("start reload")
     global jitsi
-    global poll
-    global pad
-    global codimd
-    global cryptpad
-    global etherpad
-    global ethercalc
-    global bbb
-    jitsi.renew()
-    poll.renew()
-    pad.renew()
-    codimd.renew()
-    cryptpad.renew()
-    etherpad.renew()
-    ethercalc.renew()
-    bbb.renew()
+    #global poll
+    #global pad
+    #global codimd
+    #global cryptpad
+    #global etherpad
+    #global ethercalc
+    #global bbb
+    #jitsi.renew()
+    #poll.renew()
+    #pad.renew()
+    #codimd.renew()
+    #cryptpad.renew()
+    #etherpad.renew()
+    #ethercalc.renew()
+    #bbb.renew()
     print("finish reload")
 
 uwsgi.register_signal(99, "", reload)
